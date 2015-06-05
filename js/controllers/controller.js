b1148e11baa305fd5ebd1318b89accb2f8c4f66e
Cameron Villers <cameron@ville.rs>
2015-06-05 00:20:58 -0400

    Playing with TypeScript classes for controllers

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,17 +1,54 @@
 /// <reference path="../angular.d.ts" />
 /// <reference path="../services/buttons.ts" />
-angular.module("IRControlApp").controller("ControllerController", ["$scope", "IRButtons",
-    function ($scope, buttonService) {
-        var buttonRecords = buttonService.GetButtons();
-        $scope.Buttons = {};
-        angular.forEach(buttonRecords, function (br) {
-            $scope.Buttons[br.Id] = br;
+var ControllerController = (function () {
+    function ControllerController(scope, buttonService) {
+        var _this = this;
+        var self = this;
+        this._scope = scope;
+        this._buttonService = buttonService;
+        this._buttons = buttonService.GetButtons();
+        this._buttonMap = {};
+        angular.forEach(this._buttons, function (br) {
+            _this._buttonMap[br.Id] = br;
         });
-        $scope.ControlState = { Power: buttonService.GetPowerState(), Mode: buttonService.GetFanMode() };
-        $scope.$watch(function (s) { return s.ControlState.Power; }, function (n, o, s) { return buttonService.SetPowerState(n); });
-        $scope.$watch(function (s) { return s.ControlState.Mode; }, function (n, o, s) { return buttonService.SetFanMode(n); });
-        $scope.PushButton = function (name) {
-            buttonService.PushButton($scope.Buttons[name]);
-        };
-    }]);
+        this._scope.Buttons = this._buttonMap;
+        this._scope.ControlState = { Power: buttonService.GetPowerState(), Mode: buttonService.GetFanMode() };
+        //this._scope.PushButton = n => this._buttonService.PushButton(this._buttonMap[n]);
+        //this._scope.PushButton = this.PushButton;
+        this._scope.PushButton = function (n) { return _this.PushButton(self, n); };
+        this._scope.$watch(function (s) { return s.ControlState.Power; }, function (n, o, s) { return buttonService.SetPowerState(n); });
+        this._scope.$watch(function (s) { return s.ControlState.Mode; }, function (n, o, s) { return buttonService.SetFanMode(n); });
+    }
+    /**
+     * Handler for a button push.
+     */
+    ControllerController.prototype.PushButton = function (self, name) {
+        self._buttonService.PushButton(this._buttonMap[name]);
+    };
+    return ControllerController;
+})();
+angular.module("IRControlApp").controller("ControllerController", ["$scope", "IRButtons", ControllerController]);
+/*angular.module("IRControlApp").controller("ControllerController", ["$scope", "IRButtons",
+function($scope: IControllerScope, buttonService: IButtonService)
+{
+    var buttonRecords = buttonService.GetButtons();
+
+    $scope.Buttons = {};
+
+    angular.forEach(buttonRecords, (br : Button) =>
+    {
+        $scope.Buttons[br.Id] = br;
+    });
+
+    $scope.ControlState = { Power: buttonService.GetPowerState(), Mode: buttonService.GetFanMode() };
+    
+    $scope.$watch(s => (<IControllerScope>s).ControlState.Power, (n, o, s) => buttonService.SetPowerState(n));
+    
+    $scope.$watch(s => (<IControllerScope>s).ControlState.Mode, (n, o, s) => buttonService.SetFanMode(n));
+    
+    $scope.PushButton = (name: string) =>
+    {
+        buttonService.PushButton($scope.Buttons[name])
+    };
+}]);*/ 
 //# sourceMappingURL=controller.js.map
--- js/controllers/controller.ts
+++ js/controllers/controller.ts
@@ -16,7 +16,54 @@ interface IControllerScope extends ng.IScope
 	PushButton : (name: string) => void;
 }
 
-angular.module("IRControlApp").controller("ControllerController", ["$scope", "IRButtons",
+class ControllerController
+{
+	private _scope: IControllerScope;
+	private _buttonService: IButtonService;
+	
+	private _buttons: Button[];
+	private _buttonMap: { [ id: string ]: Button };
+
+	public constructor(scope: IControllerScope, buttonService: IButtonService)
+	{
+		var self = this;
+
+		this._scope = scope;
+		this._buttonService = buttonService;
+		
+		this._buttons = buttonService.GetButtons();
+		this._buttonMap = {};
+
+		angular.forEach(this._buttons, (br : Button) =>
+		{
+			this._buttonMap[br.Id] = br;
+		});
+		
+		this._scope.Buttons = this._buttonMap;
+		
+		this._scope.ControlState = { Power: buttonService.GetPowerState(), Mode: buttonService.GetFanMode() };
+		
+		//this._scope.PushButton = n => this._buttonService.PushButton(this._buttonMap[n]);
+		//this._scope.PushButton = this.PushButton;
+		this._scope.PushButton = n => this.PushButton(self, n);
+		
+		this._scope.$watch(s => (<IControllerScope>s).ControlState.Power, (n, o, s) => buttonService.SetPowerState(n));
+	
+		this._scope.$watch(s => (<IControllerScope>s).ControlState.Mode, (n, o, s) => buttonService.SetFanMode(n));
+	}
+	
+	/**
+	 * Handler for a button push.
+	 */
+	private PushButton(self: ControllerController, name: string) : void
+	{
+		self._buttonService.PushButton(this._buttonMap[name]);
+	}
+}
+
+angular.module("IRControlApp").controller("ControllerController", ["$scope", "IRButtons", ControllerController]);
+
+/*angular.module("IRControlApp").controller("ControllerController", ["$scope", "IRButtons",
 function($scope: IControllerScope, buttonService: IButtonService)
 {
 	var buttonRecords = buttonService.GetButtons();
@@ -38,4 +85,4 @@ function($scope: IControllerScope, buttonService: IButtonService)
 	{
 		buttonService.PushButton($scope.Buttons[name])
 	};
-}]);
+}]);*/
